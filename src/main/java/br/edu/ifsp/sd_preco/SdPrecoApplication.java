b6bcb2399d814cabd9cfabfaadbbe721d7bac3bc
Luciano Araujo <luciano.ricardo@aluno.ifsp.edu.br>
2026-04-16 17:50:03 -0300

    Add files via upload

---
+++ src/main/java/br/edu/ifsp/sd_preco/SdPrecoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.sd_preco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdPrecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdPrecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/sd_preco/SdPrecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.sd_preco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdPrecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
