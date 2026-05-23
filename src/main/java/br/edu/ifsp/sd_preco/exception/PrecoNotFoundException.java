eeb68065e337f3c3d8d3b4d6791171fa8e5f3588
Luciano Araujo <araujo.luciano1302@gmail.com>
2026-05-23 13:52:16 -0300

    Add logging to PrecoNotFoundException constructor for improved error tracking

--- src/main/java/br/edu/ifsp/sd_preco/exception/PrecoNotFoundException.java
+++ src/main/java/br/edu/ifsp/sd_preco/exception/PrecoNotFoundException.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND) // faz o Spring retornar 404 automaticamente
 public class PrecoNotFoundException extends RuntimeException {
     public PrecoNotFoundException(Long id) {
-        log.info("Preço não encontrado para produtoId: {}", id);
         super("Preço não encontrado para o produto id=" + id);
+        log.info("Preço não encontrado para produtoId: {}", id);
     }
 }
