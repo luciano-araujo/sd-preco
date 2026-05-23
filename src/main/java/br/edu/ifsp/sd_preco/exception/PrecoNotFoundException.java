60a6923a4c6eba98d90cacbbef746533566640ff
Luciano Araujo <araujo.luciano1302@gmail.com>
2026-05-23 13:21:26 -0300

    Add logging to PrecoNotFoundException and PrecoService for better traceability

--- src/main/java/br/edu/ifsp/sd_preco/exception/PrecoNotFoundException.java
+++ src/main/java/br/edu/ifsp/sd_preco/exception/PrecoNotFoundException.java
@@ -1,11 +1,14 @@
 package br.edu.ifsp.sd_preco.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Slf4j
 @ResponseStatus(HttpStatus.NOT_FOUND) // faz o Spring retornar 404 automaticamente
 public class PrecoNotFoundException extends RuntimeException {
     public PrecoNotFoundException(Long id) {
+        log.info("Preço não encontrado para produtoId: {}", id);
         super("Preço não encontrado para o produto id=" + id);
     }
 }
--- src/main/java/br/edu/ifsp/sd_preco/service/PrecoService.java
+++ src/main/java/br/edu/ifsp/sd_preco/service/PrecoService.java
@@ -5,12 +5,14 @@ import br.edu.ifsp.sd_preco.exception.PrecoNotFoundException;
 import br.edu.ifsp.sd_preco.model.Preco;
 import br.edu.ifsp.sd_preco.repository.PrecoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PrecoService {
@@ -18,13 +20,16 @@ public class PrecoService {
     private final PrecoRepository precoRepository;
 
     public PrecoResponseDTO getPreco(Long id) {
+        log.info("Consultando preço para produtoId: {}", id);
         Preco preco = precoRepository.findById(id)
                 .orElseThrow(() -> new PrecoNotFoundException(id));
 
+        log.info("Preço encontrado para produtoId: {}: {}", id, preco.getValor());
         return new PrecoResponseDTO(preco.getProdutoId(), preco.getValor());
     }
 
     public Map<Long, Double> getPrecoLote(List<Long> ids) {
+        log.info("Consultando preços para os IDs: {}", ids);
         return precoRepository.findAllByIds(ids)
                 .entrySet()
                 .stream()
